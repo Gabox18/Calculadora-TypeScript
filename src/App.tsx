7b1532104d0f1b15aa263de47f32a4e627cf3571
Gabriel Ferrer <gefv1891@gmail.com>
2023-10-03 21:42:45 -0400

    layaut de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import { Box, Heading } from '@chakra-ui/react'
+import './App.css'
 
 function App() {
 	return (
-		<Box bg={'brand.terciario'}>
+		<Box>
 			<Heading textAlign={'center'} margin={'1rem auto'}>
 				Calculadora TypeScript + Chakra UI
 			</Heading>
-      <Box>
-        <label></label>
-      </Box>
+			<Box
+				w={'35%'}
+				h={'85vh'}
+				bg={'brand.secondary'}
+				m={'auto'}
+				borderRadius={'5%'}
+				paddingTop={'1.5rem'}
+			>
+				<label className='cal_label'></label>
+				<Box
+					p={'1,5rem'}
+					outline={'red solid 1px'}
+					width={'80%'}
+					h={'80%'}
+					margin={'0 auto'}
+					borderRadius={'10px'}
+				>
+					
+				</Box>
+			</Box>
 		</Box>
 	)
 }
